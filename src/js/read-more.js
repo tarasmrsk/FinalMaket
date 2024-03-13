dba8f302a00cdbe3e57b8eaf9097a5a5fd8b80d9
Andrey Tarasov <tarasmrsk@gmail.com>
2024-03-13 16:22:22 +0900

    readMoreBtn

--- src/js/read-more.js
+++ src/js/read-more.js
@@ -1,6 +1,18 @@
-let btnRead = document.querySelector('.buttonRead');
-let openRead= document.querySelector('.article__text');
+const articleText = document.querySelector('.article__text');
+const readMoreBtn = document.querySelector('.buttonRead');
 
-btnRead.onclick = function () {
-  openRead.classList.toggle('article__text--all'); 
+readMoreBtn.onclick = function () {
+  articleText.classList.toggle('article__text--all'); 
+  readMoreBtn.style.display = 'none';
 }
+
+function handleResize() {
+  if (window.innerWidth > 768) {
+    articleText.classList.remove('article__text--all');
+    readMoreBtn.style.display = 'block';
+  } 
+}
+
+window.addEventListener('resize', handleResize);
+
+handleResize();
